7b9d33083402c7cfbd80336be0634e05e859a2fc
Rekin Barışıcı <rekin.barisci@fegames.co>
2022-04-05 21:59:28 +0300

    #Enemy movement and enemy spawn

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     // Start is called before the first frame update
+    public float speed = 1f;
+    private GameObject player;
     void Start()
     {
-        
+        player = FindObjectOfType<PlayerMovement>().gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
 }
--- Assets/Scripts/MobSpawn.cs
+++ Assets/Scripts/MobSpawn.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class MobSpawn : MonoBehaviour
 {
     // Start is called before the first frame update
+    public List<Transform> spawnPositions;
+    public int  spawnDelay;
+    public GameObject enemy;
+
     void Start()
     {
-        
+        StartCoroutine(SpawnEnemiesWithDelay());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnEnemiesWithDelay()
     {
-        
+        while (true)
+        {
+            var position = Random.Range(0, spawnPositions.Count);
+            var newEnemy = Instantiate(enemy, spawnPositions[position].transform.position , Quaternion.identity);
+            yield return new WaitForSeconds(spawnDelay);
+        };
     }
 }
