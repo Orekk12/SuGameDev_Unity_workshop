91a60d3359a4bd86a6ff0588afd93e79e75d0239
Rekin Barışıcı <rekin.barisci@fegames.co>
2022-05-12 19:16:11 +0300

    #enemy and player health bars added

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,18 +6,33 @@ public class Health : MonoBehaviour
 {
     public int maxHP = 100;
     public int hp;
+    public GameObject healthBar; 
 
     public bool canTakeDmg = true;
+    
     // Start is called before the first frame update
     void Start()
     {
         hp = maxHP;
     }
 
+    void UpdateHealthBar(int dmg)
+    {
+        float oneHpScale = 0.100f / maxHP; //1 hp ye eşdeğer scale değişimini hesaplıyoruz
+        healthBar.transform.localScale += Vector3.right * oneHpScale * dmg;
+    }
+
+    void SetHealthBar(int value)
+    {
+        var newX = 0.001f * value;
+        healthBar.transform.localScale = new Vector3(newX, 0.01f, 1f);
+    }
+
     public void TakeDamage(int dmg)
     {
         hp -= dmg;
-
+        UpdateHealthBar(-dmg);
+        
         if (hp <= 0)
         {
             Die();
@@ -27,9 +42,11 @@ public class Health : MonoBehaviour
     public void Heal(int amt)
     {
         hp += amt;
-
+        UpdateHealthBar(amt);
+        
         if (hp >= maxHP)
         {
+            SetHealthBar(maxHP);
             hp = maxHP;
         }
     }
